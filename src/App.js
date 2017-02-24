8e4a7707f4c67ae41a469921bda4a9713ef9ef54
Eric Butler <eric.butler.000@gmail.com>
2017-02-23 22:39:32 -0500

    Trying to add a date library is horrible

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
+import Hours from './Hours';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-background">
-          <img src="https://s3.us-east-2.amazonaws.com/restaurant.ericbutler.info/Restaurant.JPG" className="App-logo" alt="logo" />
-        </div>
         <section className="App-content">
-			Welcome to <i>React Ristorante</i>
+          <span className="slide-in">Welcome to <i>React Ristorante</i></span>
         </section>
+        <Hours />
       </div>
     );
   }
